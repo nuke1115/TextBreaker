cdd146d4c7a35099988786c5dc466ba1ba665628
윤석 이 <nuclearbomb280@gmail.com>
2025-08-07 18:00:02 +0900

    feature : added printf and scanf functions

---
+++ TextBreaker/Include/IO/ConsoleIOManager.hpp
@@ -0,0 +1,32 @@
+#ifndef TEXT_GAME_ENGINE_CONSOLE_IO_MANAGER_HEADER
+#define TEXT_GAME_ENGINE_CONSOLE_IO_MANAGER_HEADER
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <Windows.h>
+#include <stdint.h>
+#include "./Colors.hpp"
+
+
+namespace TextGameEngine
+{
+    namespace IO
+    {
+        class ConsoleIOManager
+        {
+        private:
+            HANDLE _stdIn = NULL;
+            HANDLE _stdOut = NULL;
+        public:
+            ConsoleIOManager();
+            ConsoleIOManager(const ConsoleIOManager&) = delete;
+            ConsoleIOManager(ConsoleIOManager&&) = delete;
+
+            int PrintToConsole(const char* const format, ...);
+            int ScanFromConsole_s(const char* const format, ...);
+
+        };
+    }
+}
+
+#endif // !TEXT_GAME_ENGINE_CONSOLE_IO_MANAGER_HEADER
---
+++ TextBreaker/Src/IO/ConsoleIOManager.cpp
@@ -0,0 +1,30 @@
+#include "../../Include/IO/ConsoleIOManager.hpp"
+
+TextGameEngine::IO::ConsoleIOManager::ConsoleIOManager()
+{
+    _stdIn = GetStdHandle(STD_INPUT_HANDLE);
+    _stdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+}
+
+
+int TextGameEngine::IO::ConsoleIOManager::PrintToConsole(const char* const format, ...)
+{
+    va_list args;
+    va_start(args, format);
+
+    int retValue = vprintf(format, args);
+
+    va_end(args);
+    return retValue;
+}
+
+int TextGameEngine::IO::ConsoleIOManager::ScanFromConsole_s(const char* const format, ...)
+{
+    va_list args;
+    va_start(args, format);
+
+    int retValue = vscanf_s(format, args);
+
+    va_end(args);
+    return retValue;
+}
