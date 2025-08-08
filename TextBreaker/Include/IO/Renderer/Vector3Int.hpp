6913d55791498da5932ac1b0ad7ee981b66013ff
윤석 이 <nuclearbomb280@gmail.com>
2025-08-08 23:26:43 +0900

    feature : added Vector3Int class

---
+++ TextBreaker/Include/IO/Renderer/Vector3Int.hpp
@@ -0,0 +1,20 @@
+#ifndef TEXT_GAME_ENGINE_VECTOR_3_INT_HEADER
+#define TEXT_GAME_ENGINE_VECTOR_3_INT_HEADER
+
+#include <Windows.h>
+
+namespace TextGameEngine
+{
+    namespace IO
+    {
+        namespace ConsoleRenderer
+        {
+            struct Vector3Int
+            {
+                int x, y, z;
+            };
+        }
+    }
+}
+
+#endif // !TEXT_GAME_ENGINE_VECTOR_3_INT_HEADER
